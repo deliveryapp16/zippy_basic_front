83b4aa9760784f017ba1cb5e44c7accfdc0473f6
Sebastian Monsalve <sebastiandev16@gmail.com>
2025-06-27 16:51:17 -0500

    Update test label in homepage to include author name

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="w-screen h-screen">
       <div className="w-full h-full flex flex-col justify-center items-center">
         <img src="/images/home.png" alt="hero" />
-        <div className="pt-6">TEST</div>
+        <div className="pt-6">TEST - sebas</div>
       </div>
     </div>
   );
