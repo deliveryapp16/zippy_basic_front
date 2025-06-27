558dc68e8c93c99de3be5911d631ad1456e680ac
GabrielOque <oquendodev@gmail.com>
2025-06-26 21:55:57 -0500

    test deploy test

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div className="w-screen h-screen">
-      <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full h-full flex flex-col justify-center items-center">
         <img src="/images/home.png" alt="hero" />
+        <div className="pt-6">TEST</div>
       </div>
     </div>
   );
